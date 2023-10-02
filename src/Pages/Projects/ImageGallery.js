2b1cc266f1455fc41103af4eff6407ec0b79366b
Luis <lvas248@gmail.com>
2023-10-01 23:07:56 -0400

    more animation limitations

--- src/Pages/Projects/ImageGallery.js
+++ src/Pages/Projects/ImageGallery.js
@@ -6,7 +6,7 @@ function ImageGallery({images, cardSelected, toggleCardSelected}) {
     const [ selectedImage, setSelectedImage ] = useState(images[0].image)
 
     const renderSmallImages = images.map( i =>{
-        return <button key={i.id} onClick={()=>setSelectedImage(i.image)}> <img loading='lazy' className={`h-[30px] sm:h-[50px] border-white border-2 hover:animate-scale-large animate-fade-in-slow`} alt={i.id} src={i.image} /></button>
+        return <button key={i.id} onClick={()=>setSelectedImage(i.image)}> <img loading='lazy' className={`h-[30px] sm:h-[50px] border-white border-2 hover:sm:animate-scale-large`} alt={i.id} src={i.image} /></button>
     })
 
     function close(){
@@ -19,7 +19,7 @@ function ImageGallery({images, cardSelected, toggleCardSelected}) {
 
             <img onClick={close} alt='large' className={`max-h-[40vh] w-full m-auto border-white border-2 ${!cardSelected && 'grayscale hover:sm:animate-scale-up'} hover:sm:cursor-pointer `}  src={selectedImage} />
 
-            <div className={`flex flex-wrap px-5 gap-2 m-auto ${!cardSelected && 'hidden'}`}>
+            <div className={`flex flex-wrap px-5 gap-2 m-auto ${!cardSelected && 'hidden'}  animate-fade-in`}>
                 {renderSmallImages}
             </div>
 
