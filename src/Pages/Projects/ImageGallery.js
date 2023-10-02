bfa9178c1eac840ee4c91658ee10887f1cc3540a
Luis <lvas248@gmail.com>
2023-10-01 22:46:35 -0400

    add fade in animation to cards

--- src/Pages/Projects/ImageGallery.js
+++ src/Pages/Projects/ImageGallery.js
@@ -6,7 +6,7 @@ function ImageGallery({images, cardSelected, toggleCardSelected}) {
     const [ selectedImage, setSelectedImage ] = useState(images[0].image)
 
     const renderSmallImages = images.map( i =>{
-        return <button key={i.id} onClick={()=>setSelectedImage(i.image)}> <img className={`h-[30px] sm:h-[50px] border-white border-2 hover:animate-scale-large`} alt={i.id} src={i.image} /></button>
+        return <button key={i.id} onClick={()=>setSelectedImage(i.image)}> <img loading='lazy' className={`h-[30px] sm:h-[50px] border-white border-2 hover:animate-scale-large`} alt={i.id} src={i.image} /></button>
     })
 
     function close(){
@@ -19,7 +19,7 @@ function ImageGallery({images, cardSelected, toggleCardSelected}) {
 
             <img onClick={close} alt='large' className={`max-h-[40vh] w-full m-auto border-white border-2 ${!cardSelected && 'grayscale hover:animate-scale-up'} hover:cursor-pointer `}  src={selectedImage} />
 
-            <div className='flex flex-wrap px-5 gap-2 m-auto '>
+            <div className={`flex flex-wrap px-5 gap-2 m-auto ${!cardSelected && 'hidden'}`}>
                 {renderSmallImages}
             </div>
 
